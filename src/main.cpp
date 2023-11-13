2c5e281fe97af1c6bbad6d810e3027fe47d4ff2f
Ferreira-Orlann <ferreira.orlann@gmail.com>
2023-11-13 18:03:18 +0100

    Ajoutez des fichiers projet.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// Porjet Fin Module.cpp : définit le point d'entrée de l'application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
